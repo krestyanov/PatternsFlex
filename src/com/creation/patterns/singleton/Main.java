410e2b24b51681b24297d015aa4133073741755f
krestyanov <i.krestyanov7@gmail.com>
2019-04-18 23:50:38 +0300

    Update Main.java

--- src/com/creation/patterns/singleton/Main.java
+++ src/com/creation/patterns/singleton/Main.java
@@ -1,11 +1,11 @@
 package com.creation.patterns.singleton;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        MyHome home = MyHome.getInstance();
-        System.out.println(home.getRoom());
-        home.setRoom(15);
-        MyHome home1 = MyHome.getInstance();
-        System.out.println(home1.getRoom());
+
+        MyHome.INSTANCE.printName();
+
+        System.out.println(MyHome.INSTANCE.getClass());
     }
 }
+
